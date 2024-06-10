262c5159bbceb8e6813f6225b94b1f1510085e05
Saiful Islam <saiful islam>
2024-06-10 15:15:47 +0600

    added user request to admin api

--- index.js
+++ index.js
@@ -39,10 +39,12 @@ async function run() {
     try {
 
         const usersCollection = client.db("bangalTourDb").collection("users");
+        const requestToAdminCollection = client.db("bangalTourDb").collection("requestToAdmin");
         const wishListCollection = client.db("bangalTourDb").collection("wishList");
         const tourGuideCollection = client.db("bangalTourDb").collection("tourGuide");
         const packagesCollection = client.db("bangalTourDb").collection("packages");
         const packageBookingCollection = client.db("bangalTourDb").collection("packageBooking");
+
 
         // jwt related api
         app.post('/jwt', async (req, res) => {
@@ -51,11 +53,100 @@ async function run() {
             res.send({ token })
         })
 
+        // middleware jwt
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if (error) {
+                    return res.status(401).send({ message: 'unauthorized access' })
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+        // check isAdmin  or not middleware
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
+
+
         // users api
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyToken, async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
+
+        app.get("/users", verifyToken, async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const { name, role } = req.query;
+            let query = {};
+            if (name) {
+                query.name = { $regex: name, $options: "i" };
+            }
+            if (role) {
+                query.role = role;
+            }
+            const result = await usersCollection
+                .find(query)
+                .skip((page - 1) * size)
+                .limit(size)
+                .toArray();
+            res.send(result);
+        });
+
+        // getting users length for pagination
+
+        app.get("/users/count", async (req, res) => {
+            const result = await usersCollection.estimatedDocumentCount();
+            res.send({ count: result });
+        });
+        app.get('/users/:email', verifyToken, async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
+            const result = await usersCollection.findOne(query)
+            res.send(result)
+        })
+
+        // check admin or not
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let admin = false;
+            if (user) {
+                admin = user?.role === "admin"
+            }
+            res.send({ admin })
+        })
+        app.get('/users/guide/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            const query = { email: email }
+            const user = await usersCollection.findOne(query)
+            let guide = false;
+            if (user) {
+                guide = user?.role === "guide"
+            }
+            res.send({ guide })
+        })
+
+        // set user on signup
         app.post('/users', async (req, res) => {
             const user = req.body
             // check user isexists in dataabse
@@ -71,6 +162,32 @@ async function run() {
         })
 
 
+
+        // request-to-admin api
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user?.email }
+            const alreadyRequested = await usersCollection.findOne(query)
+            if (alreadyRequested) {
+                if (user.roleStatus === 'requested') {
+                    const result = await usersCollection.updateOne(query, { $set: { roleStatus: user?.roleStatus } })
+                    return res.send(result)
+                } else {
+
+                    return res.send({ insertedId: null })
+                }
+            }
+            const option = { upsert: true }
+            const updateDoc = {
+                $set: {
+                    ...user,
+                }
+            }
+            const result = await usersCollection.insertOne(query, updateDoc, option)
+            res.send(result)
+
+        })
+
         // packages api
         app.get('/packages', async (req, res) => {
             const result = await packagesCollection.find().toArray();
@@ -82,7 +199,7 @@ async function run() {
             const result = await packagesCollection.findOne(query);
             res.send(result)
         })
-        app.post('/packages', async (req, res) => {
+        app.post('/packages', verifyToken, verifyAdmin, async (req, res) => {
             const package = req.body;
             const result = await packagesCollection.insertOne(package);
             res.send(result)
@@ -108,11 +225,35 @@ async function run() {
         })
 
         // booking api
+        app.get('/booking/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { userEmail: email }
+            const result = await packageBookingCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.put("/booking", verifyToken, async (req, res) => {
+            const { id, status } = req.query;
+            const filter = { _id: new ObjectId(id) };
+            const updateBooking = {
+                $set: {
+                    status: status,
+                },
+            };
+            const options = { upsert: true };
+            const result = await packageBookingCollection.updateOne(filter, updateBooking, options);
+            res.send(result);
+        });
+        app.get("/bookings/:email", verifyToken, async (req, res) => {
+            const email = req.params.email;
+            const result = await packageBookingCollection.find({ email: email }).toArray();
+            res.send(result);
+        }
+        );
         app.post('/booking', async (req, res) => {
             const booking = req.body;
             const query = {
 
-                packageName: booking.packageName,
+                name: booking.name,
                 userEmail: booking.userEmail
             }
             const alreadyBooked = await packageBookingCollection.findOne(query)
@@ -124,31 +265,47 @@ async function run() {
             const result = await packageBookingCollection.insertOne(booking);
             res.send(result)
         })
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await packageBookingCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // wishlist
-        app.get('/wishList/:email', async(req,res)=>{
+        app.get('/wishList/:email', async (req, res) => {
             const email = req.params.email;
-            const query = {email: email}
+            const query = { userEmail: email }
             const result = await wishListCollection.find(query).toArray()
             res.send(result)
         })
+
+        app.get('/wishList/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) };
+            const result = await wishListCollection.findOne(query);
+            res.send(result)
+        })
+
 
         app.post('/wishList', async (req, res) => {
             const package = req.body;
             const query = {
                 name: package.name,
-                email: package.email
+                userEmail: package.userEmail
             }
             const alreadyListed = await wishListCollection.findOne(query)
-            if(alreadyListed){
+            if (alreadyListed) {
                 return res.status(400).send('Already listed')
             }
             const result = await wishListCollection.insertOne(package);
             res.send(result)
         })
 
-        app.delete('/wishList/:id', async (req,res)=>{
+        app.delete('/wishList/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await wishListCollection.deleteOne(query)
             res.send(result)
         })
